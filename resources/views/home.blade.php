3df5c38066a7290c248244aec48adb9a3478dafe
Jason Sekhon <jason_2s@hotmail.com>
2017-04-17 22:14:19 -0700

    initial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
+        <div id="header" class="row text-center content-wrapper set-border">
+            <div>
+                <h2 class="set-inline"><span>{{Auth::user()->email}}</span>
+                    <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a></h2>
+            </div>
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
+        </div>
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
